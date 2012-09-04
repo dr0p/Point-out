2d8272cc9d1a763338f32bea4c9f87367903f52e
dr0p <pawdro@gmail.com>
2012-09-04 21:40:14 +0200

    1n1t commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PointOut::Application.config.secret_token = 'e457c96b648932702b869b6e3070c88e9b7de574265ba763e5a0de0edfdc7b05bdfc652d81a5919cce6b720476e4cdedbc28780421cfde565ca80843456c9e44'
