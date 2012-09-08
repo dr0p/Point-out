5eb29f97492649cc104a7baff14fe9c0bcdaa254
Paweł Drożański <dr0p@Divine.(none)>
2012-09-08 18:30:09 +0200

    main page rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PointOut::Application.routes.draw do
+
   resources :issues
   resources :users
   resources :sessions
@@ -6,5 +7,7 @@ PointOut::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  #root :to => "users#new"
+  root :to => "sessions#new"
+
 end
