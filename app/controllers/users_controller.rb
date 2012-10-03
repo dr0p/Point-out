b831f0d8b1d212a340e0bf3b251e401a9b3818d9
Paweł Drożański <dr0p@T420.(none)>
2012-10-03 11:20:30 +0200

    redirection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       #redirect_to root_url, :notice => "Signed up!"
-      redirect_to issues_url
+      #redirect_to issues_url
+      redirect_to log_in_url
     else
       render "new"
     end
