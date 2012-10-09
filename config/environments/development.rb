b2e93cf4999023bca7c03a41d95d6906cff25876
Paweł Drożański <pawdro@gmail.com>
2012-10-09 14:56:34 +0200

    mail - localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,8 @@ PointOut::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "localhost",
+    #:address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'PointIt',
     :user_name            => 'point.it.mail@gmail.com',
